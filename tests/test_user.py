2b4086e51a6c3232af91785e6ac887dd6127bf59
Polaryyy <polaryyyyypolina.larionova.00@mail.ru>
2026-09-09 19:49:26 +0300

    New in the tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,7 @@
 def test_user_name():
-    user = {"name": "Lola", "job": "QA Engineer"}
+    user = {"name": "Lola", 
+            "job": "QA Engineer"
+            }
 
     assert user["name"] == "Lola"
+    assert user["job"] == "QA"
