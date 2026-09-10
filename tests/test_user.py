45672cf64d8d68d9801608068e3951d31a04240d
Polaryyy <polaryyyyypolina.larionova.00@mail.ru>
2026-09-10 09:28:31 +0300

    add new test job

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,15 @@
 def test_user_name():
-    user = {"name": "Lola", 
-            "job": "QA Engineer"
-            }
+    user = {
+        "name": "Lola", 
+        "job": "QA Engineer"
+        }
 
     assert user["name"] == "Lola"
-    assert user["job"] == "QA"
+
+
+def test_user_job():
+      user = {
+                "name": "Lola", 
+                "job": "QA Engineer"
+        }
+      assert user["job"] == "QA"
